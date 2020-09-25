9287f041aa93d5183ed90edca66ffbdc70885548
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 12:00:18 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+ attr_accessor :name, :songs
+
+
+
+ def initialize(name, songs)
+   @name = name
+   @songs = []
+ end
+
+ def add_song(song)
+   self.songs << song
+   song.artist = self
+   @@song_count += 1
+ end
+
+
+
+
+
+
+
+
+
+
+
+
+end
