f1508fe7f55bd2416928cf3c14f475822bdd8234
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 12:42:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,33 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
+
+  @@song_count = 0
 
   def initialize(name)
     @name = name
   end
 
   def songs
-
+    @songs   
   end
+
+  def add_song(song)
+    @song << song 
+    @@song_count += 1 
+    song.artist = self
+  end 
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    @@song_count += 1
+    song.artist = self 
+  end 
+
+  def self.song_count
+    @@song_count
+  end 
+end  
 
 #   def add_song(song)
 #     song.artist = self
