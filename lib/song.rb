7d090c743613eadb345fe8633e4a7d9c407a69d6
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 13:01:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,21 +2,25 @@ class Song
   attr_accessor :artist, :name
 
   @@all = []
-
+  
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  # def save
-  #   @@all << self
-  # end
+  def artist_name
+    if @artist.name == nil 
+      return nil
+    else 
+      @artist.name
+    end 
+  end 
 
-  def self.all
-    @@all
-  end
+   def self.all 
+     @@all 
+  end 
+end 
 
-  #def artist_name
+  
 
-
-end
+  
