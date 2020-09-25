08df4b6997932cc43d93217825c1f9b2ef30c3f4
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 12:42:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,26 +8,26 @@ class Artist
   end
 
   def songs
-    @songs   
+    @songs
   end
 
   def add_song(song)
-    @song << song 
-    @@song_count += 1 
+    @song << song
+    @@song_count += 1
     song.artist = self
-  end 
+  end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song 
+    @songs << song
     @@song_count += 1
-    song.artist = self 
-  end 
+    song.artist = self
+  end
 
   def self.song_count
     @@song_count
-  end 
-end  
+  end
+end
 
 #   def add_song(song)
 #     song.artist = self
