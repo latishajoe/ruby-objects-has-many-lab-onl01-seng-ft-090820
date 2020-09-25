4bc3ccf24adbcb704939f58b4e9fabfb647b162a
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 12:43:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,14 +16,14 @@ class Artist
 #     @@song_count += 1
 #     song.artist = self
 #   end
-# 
+#
 #   def add_song_by_name(name)
 #     song = Song.new(name)
 #     @songs << song
 #     @@song_count += 1
 #     song.artist = self
 #   end
-# 
+#
 #   def self.song_count
 #     @@song_count
 #   end
