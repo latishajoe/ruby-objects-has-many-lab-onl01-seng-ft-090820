ad73afaddcd2ce3bdd4aaad477cb364d3499f68d
Latisha Joe <latisha_joe@yahoo.com>
2020-09-25 13:47:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,21 @@
 class Author
   attr_accessor :name
 
+@@post_count = 0
+
  def initialize(name)
    @name = name
+   @posts = []
  end
 
  def posts
    Post.all.select {|post| post.author == self}
+ end
+
+ def add_post(post)
+   @posts << post
+   post.author = self
+   @@post_count += 1
  end
 
 
